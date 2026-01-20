98afb2fe4e9cac56b620538db5a5711159566915
james ong <burkedev@gmail.com>
2026-01-20 21:31:24 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <div class="pt-safe-top bg-blue-500 text-white">
+    This header won't be hidden by the notch.
+  </div>
+</template>
+
+<style scoped></style>
