7984f0492ba3269cd0c33fdedbeb8b7da6629cc8
james ong <burkedev@gmail.com>
2026-01-28 20:54:16 +0800

    fb login fix

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -12,6 +12,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure() // Initialize Firebase
+
+        ApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
         return true
     }
 
