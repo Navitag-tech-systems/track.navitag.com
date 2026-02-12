2eebf95b3e49a46c758ad1bcc6a379a8e79f4c2b
james ong <burkedev@gmail.com>
2026-02-12 19:59:00 +0800

    add capasitor firebase analytics

--- ios/App/CapApp-SPM/Package.swift
+++ ios/App/CapApp-SPM/Package.swift
@@ -12,10 +12,11 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "8.0.1"),
-        .package(name: "CapacitorFirebaseAuthentication", path: "../../../node_modules/@capacitor-firebase/authentication"),
-        .package(name: "CapacitorFirebaseMessaging", path: "../../../node_modules/@capacitor-firebase/messaging"),
-        .package(name: "CapacitorApp", path: "../../../node_modules/@capacitor/app"),
-        .package(name: "CapacitorGeolocation", path: "../../../node_modules/@capacitor/geolocation")
+        .package(name: "CapacitorFirebaseAnalytics", path: "..\..\..\node_modules\@capacitor-firebase\analytics"),
+        .package(name: "CapacitorFirebaseAuthentication", path: "..\..\..\node_modules\@capacitor-firebase\authentication"),
+        .package(name: "CapacitorFirebaseMessaging", path: "..\..\..\node_modules\@capacitor-firebase\messaging"),
+        .package(name: "CapacitorApp", path: "..\..\..\node_modules\@capacitor\app"),
+        .package(name: "CapacitorGeolocation", path: "..\..\..\node_modules\@capacitor\geolocation")
     ],
     targets: [
         .target(
@@ -23,6 +24,7 @@ let package = Package(
             dependencies: [
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
                 .product(name: "Cordova", package: "capacitor-swift-pm"),
+                .product(name: "CapacitorFirebaseAnalytics", package: "CapacitorFirebaseAnalytics"),
                 .product(name: "CapacitorFirebaseAuthentication", package: "CapacitorFirebaseAuthentication"),
                 .product(name: "CapacitorFirebaseMessaging", package: "CapacitorFirebaseMessaging"),
                 .product(name: "CapacitorApp", package: "CapacitorApp"),
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,24 +1,18 @@
 <script setup>
 import { useUserStore } from '@/stores/user';
 
-import { signOut } from '@/utils/auth';
-import LocationPicker from '@/components/LocationPicker.vue';
-import BarcodeScanner from '@/components/BarcodeScanner.vue';
+
+//import LocationPicker from '@/components/LocationPicker.vue';
+//import BarcodeScanner from '@/components/BarcodeScanner.vue';
 
 const userStore = useUserStore();
 
-const handleLogout = async () => {
-  await signOut();
-};
 </script>
 
 <template>
+
   <div class="p-4 pt-safe-top bg-gray-50 min-h-screen">
     <h1 class="text-2xl font-bold mb-4">Navitag Track</h1>
-    
-    <LocationPicker />
-    <BarcodeScanner />
-
     <div class="mt-8 bg-white shadow rounded p-4 text-center">
       <p class="mb-4 text-sm text-gray-500">Account: {{ userStore.user?.email }}</p>
       <button @click="handleLogout" class="text-red-500 text-sm font-semibold">
